bdc704b7cab7ff457317100b9259198e4298a588
Seracrooce <fschmidt@live.com>
2021-11-17 12:09:39 -0600

    First commit

---
+++ shopping.js
@@ -0,0 +1,53 @@
+const list = [
+    milk = {
+        id: 1,
+        name: "milk",
+        price: 3
+    },
+    eggs = {
+        id: 2,
+        name: "eggs",
+        price: 40
+    },
+    pizzaRolls = {
+        id: 3,
+        name: "pizza rolls",
+        price: 12
+    },
+    porkChops = {
+        id: 4,
+        name: "porkchops",
+        price: 13
+    },
+    grapes = {
+        id: 5,
+        name: "grapes",
+        price: 10
+    },
+    buns = {
+        id: 6,
+        name: "sandwich buns",
+        price: 34
+    },
+    ketchup = {
+        id: 7,
+        name: "ketchup",
+        price: 14
+    }
+]
+
+console.log(list);
+
+function addToShoppingList(array, item, price) {
+    //length + 1 because the highest ids equal the current lenght
+    item = {
+        id: array.length + 1,
+        name: item,
+        price: price,
+        dateCreated: new Date()
+    }
+    return item;
+}
+
+const test = addToShoppingList(list, "potatoes", 4);
+console.log(test);
